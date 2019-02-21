f6936cd8f6cb885acc619168b25d2a657c827c57
Wendy <wendy.wang55@gmail.com>
2019-02-20 23:24:42 -0500

    complete,delete,logout

--- run.py
+++ run.py
@@ -1,16 +1,8 @@
 from flask import Flask, request, render_template, redirect, url_for
-from flask import g
 import os
 import requests, json
 
 app = Flask(__name__)
-ctx = app.app_context()
-ctx.push()
-
-
-@app.route('/hello')
-def hello():
-    return 'Hello World!'
 
 @app.route('/')
 def home():
@@ -20,7 +12,6 @@ def home():
 def user():
     r = requests.get('https://hunter-todo-api.herokuapp.com/user')
     return r.text
-
 
 @app.route('/register')
 def register():
@@ -46,15 +37,16 @@ def login_user():
     username = request.form['username']
     r = requests.post('https://hunter-todo-api.herokuapp.com/auth', data=json.dumps({'username': username}))
     if r.status_code == 200:
-        g.cookies = r.cookies
+        global gcookies
+        gcookies = r.cookies
         return redirect('/todo-item')
     else:
         return 'login error'
 
-@app.route('/todo-item')
+@app.route('/todo-item',methods = ['GET','POST'])
 def todo_item():
-    r = requests.get('https://hunter-todo-api.herokuapp.com/todo-item', cookies=g.cookies)
-    return render_template('todo-item.html', todo_item = r.text)
+    r = requests.get('https://hunter-todo-api.herokuapp.com/todo-item', cookies=gcookies)
+    return render_template('todo-item.html', todo_item = r.json())
 
 @app.route('/new-item')
 def new_item():
@@ -63,22 +55,18 @@ def new_item():
 @app.route('/create-item', methods=['post'])
 def create_item():
     content = request.form['content']
-    r = requests.post('https://hunter-todo-api.herokuapp.com/todo-item', data=json.dumps({'content': content}), cookies=g.cookies)
+    r = requests.post('https://hunter-todo-api.herokuapp.com/todo-item', data=json.dumps({'content': content}), cookies=gcookies)
     return redirect('/todo-item')
 
-@app.route('/completed-item', methods=['post'])
-def completed_item():
-    r = requests.post('https://hunter-todo-api.herokuapp.com/todo-item',data=json.dumps({'completed':True}),cookies=g.cookies)
+@app.route('/completed-item/<id>')
+def completed_item(id):
+    r = requests.put('https://hunter-todo-api.herokuapp.com/todo-item/' + id,data=json.dumps({'completed':True}),cookies=gcookies)
     return redirect('/todo-item')
 
-@app.route('/delete-item', methods=['post'])
-def delete_item():
-    r = requests.post('https://hunter-todo-api.herokuapp.com/todo-item',data=json.dumps({'delete':True}),cookies=g.cookies)
+@app.route('/delete-item/<id>')
+def delete_item(id):
+    r = requests.delete('https://hunter-todo-api.herokuapp.com/todo-item/' + id,data=json.dumps({'delete':True}),cookies=gcookies)
     return redirect('/todo-item')
-
-@app.route('/test')
-def test():
-    return redirect(url_for('hello'))
 
 if __name__ == "__main__":
 	port = int(os.environ.get("PORT", 5000))
